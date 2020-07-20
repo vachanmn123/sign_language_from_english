acbea61913ec6b6e634057c08276ce85d2956f88
Vachan <vachanmn123@gmail.com>
2020-07-20 21:10:11 +0530

    added images

--- sign_lang.py
+++ sign_lang.py
@@ -16,11 +16,9 @@ while i <= length - 1:
     if (word[i]) == 'a' :
         sign_word = sign_word + 'a'
         webbrowser.open('signs/a.jpg')
-        time.sleep(0.3)
     elif (word[i]) == 'b' :
         sign_word = sign_word + 'b'
         webbrowser.open('signs/b.jpg')
-        time.sleep(0.3)
     elif (word[i]) == 'c' :
         sign_word = sign_word + 'c'
         webbrowser.open('signs/c.jpg')
@@ -35,49 +33,68 @@ while i <= length - 1:
         webbrowser.open('signs/f.jpg')
     elif (word[i]) == 'g' :
         sign_word = sign_word + 'g'
+        webbrowser.open('signs/g.jpg')
     elif (word[i]) == 'h' :
         sign_word = sign_word + 'h'
+        webbrowser.open('signs/h.jpg')
     elif (word[i]) == 'i' :
         sign_word = sign_word + 'i'
+        webbrowser.open('signs/i.jpg')
     elif (word[i]) == 'j' :
         sign_word = sign_word + 'j'
+        webbrowser.open('signs/j.jpg')
     elif (word[i]) == 'k' :
         sign_word = sign_word + 'k'
+        webbrowser.open('signs/k.jpg')
     elif (word[i]) == 'l' :
         sign_word = sign_word + 'l'
+        webbrowser.open('signs/l.jpg')
     elif (word[i]) == 'm' :
         sign_word = sign_word + 'm'
+        webbrowser.open('signs/m.jpg')
     elif (word[i]) == 'n' :
         sign_word = sign_word + 'n'
+        webbrowser.open('signs/n.jpg')
     elif (word[i]) == 'o' :
         sign_word = sign_word + 'o'
+        webbrowser.open('signs/o.jpg')
     elif (word[i]) == 'p' :
         sign_word = sign_word + 'p'
+        webbrowser.open('signs/p.jpg')
     elif (word[i]) == 'q' :
         sign_word = sign_word + 'q'
+        webbrowser.open('signs/q.jpg')
     elif (word[i]) == 'r' :
         sign_word = sign_word + 'r'
+        webbrowser.open('signs/r.jpg')
     elif (word[i]) == 's' :
         sign_word = sign_word + 's'
+        webbrowser.open('signs/s.jpg')
     elif (word[i]) == 't' :
         sign_word = sign_word + 't'
+        webbrowser.open('signs/t.jpg')
     elif (word[i]) == 'u' :
         sign_word = sign_word + 'u'
+        webbrowser.open('signs/u.jpg')
     elif (word[i]) == 'v' :
         sign_word = sign_word + 'v'
+        webbrowser.open('signs/v.jpg')
     elif (word[i]) == 'w' :
         sign_word = sign_word + 'w'
+        webbrowser.open('signs/w.jpg')
     elif (word[i]) == 'x' :
         sign_word = sign_word + 'x'
+        webbrowser.open('signs/x.jpg')
     elif (word[i]) == 'y' :
         sign_word = sign_word + 'y'
+        webbrowser.open('signs/y.jpg')
     elif (word[i]) == 'z' :
         sign_word = sign_word + 'z'
-    elif (word[i]) == '1' :
-        sign_word = sign_word + '1'
+        webbrowser.open('signs/z.jpg')
     else:
         print("Invalid")
         break
+    time.sleep(2)
     i = i + 1
 
 #print the word in sign language
